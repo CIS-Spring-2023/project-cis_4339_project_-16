ca90e23f88e9feca9ae0c6a6549969e21729d447
Distroyed <91142538+Distroyed@users.noreply.github.com>
2023-02-20 18:45:37 -0600

    Create the login.vue page.  Updated index.js with new route for /login. Updated App.Vue with Login on list.

---
+++ frontend/src/components/logIn.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await axios.post('http://myApiEndpoint.com/login', {
+                    email: this.email,
+                    password: this.password
+                })
+                console.log(response.data)
+            } catch(error) {
+                console.log(error.response.data)
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent='login'>
+            <label>
+                Email:
+                <input type='email' v-model='email' required>
+            </label>
+            <label>
+                Password:
+                <input type='password' v-model='password' required>
+            </label>
+            <button type='submit' @click.prevent="login()">Login</button>
+        </form>
+    </div>
+</template>
