6b771fe5111e1587732d234b8c193a45e7a3c617
Distroyed <91142538+Distroyed@users.noreply.github.com>
2023-02-27 19:41:50 -0600

    Added some hardcoded email and password for the login page.

--- frontend/src/components/pieChart.vue
+++ frontend/src/components/pieChart.vue
@@ -1,59 +1,43 @@
 <script>
 import { Chart, registerables } from "chart.js";
-import {data} from "autoprefixer";
 Chart.register(...registerables);
 
 export default {
-    props: {
-      zipLabel: {
-        type: Array,
-      },
-      zipChartData: {
-        type: Array
-      },
+  props: {
+    label: {
+      type: Array,
+    },
+    ChartData: {
+      type: Array,
+    },
   },
   mounted() {
-    console.log(this.zipChartData);
-    const backgroundColor = this.zipChartData.map(() => this.getColor());
-    const borderColor = backgroundColor.map((e) =>
-      e.replace(/[\d\.]+\)$/g, "1)")
-    );
     new Chart(this.$refs.pieChart, {
       type: "pie",
       data: {
-        labels: this.zipLabels,
+        labels: this.label,
         datasets: [
           {
-            label: 'Does this work',
             borderWidth: 1,
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            data: this.zipChartData,
+            backgroundColor: ["maroon", "violet", "red", "grey", "yellow"],
+            borderColor: "black",
           },
         ],
       },
       options: {
         plugins: {
           legend: {
-            display: true,
-            position: "bottom",
+            display: false,
           },
         },
-
       },
     });
-  },
-  methods: {
-    getColor() {
-      let channel = () => Math.random() * 255;
-      return `rgba(${channel()}, ${channel()}, ${channel()}, 0.2)`;
-    },
   },
 };
 </script>
 
 <template>
-  <div class="shadow-lg rounded-lg">
+  <div class="shadow-lg rounded-lg overflow-hidden">
     <canvas class="p-10" ref="pieChart"></canvas>
   </div>
 </template>
