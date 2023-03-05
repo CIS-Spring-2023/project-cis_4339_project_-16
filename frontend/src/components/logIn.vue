48480ac02c45018f7aa9cceae4703bf5a638e4ce
Jeremy <jeremy.l.griffith@gmail.com>
2023-03-05 11:11:59 -0600

    Worked on positioning and styling of login page.  It is where it needs to be right now to be uniform to the overall sites theme

--- frontend/src/components/logIn.vue
+++ frontend/src/components/logIn.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="flex items-center justify-center">
-
-            <h3 class="font-bold text-4xl text-red-700 tracking-widest text-center">Login</h3>
-            <form @submit.prevent="store.login(username, password)" novalidate="true">
-                <div class="flex flex-col">
-                    <label>User name</label>
-                    <input type="text" class="w-64 border rounded px-4 py-2" v-model="username" placeholder="username" required>
-                </div>
-                <div class="flex flex-col">
-                    <label>Password</label>
-                    <input type="password" class="bg-red-700 text-white rounded" v-model="password" placeholder="password" required>
-                </div>
-                <button class="btn btn-danger mt-3">Submit</button>
-            </form>
-
+  <br>
+  <div class="flex items-center justify-center mb-5">
+    <h3 class="font-bold text-4xl text-red-700">Login</h3>
+  </div>
+  <br>
+  <div class="flex items-center justify-center mb-5">
+    <form @submit.prevent="store.login(username, password)" novalidate="true" class="grid grid-cols-1 gap-y-10">
+      <div class="flex flex-col">
+        <label class="mb-1">User name</label>
+        <input type="text" class="w-64 border rounded px-4 py-2 mb-2" v-model="username" placeholder="username" required>
+      </div>
+      <div class="flex flex-col">
+        <label class="mb-1">Password</label>
+        <input type="password" class="w-64 border rounded px-4 py-2 mb-8" v-model="password" placeholder="password" required>
+      </div>
+      <div class="flex items-center justify-center">
+        <button class="bg-red-700 text-white rounded py-2 px-4">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
+
 
 <script>
 import { useLoggedInUserStore } from "@/store/loggedInUser";
