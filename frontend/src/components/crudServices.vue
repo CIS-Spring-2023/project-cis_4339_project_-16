b6be5751921c63d882132dac290125066075227d
Eddyroxta <89483925+Eddyroxta@users.noreply.github.com>
2023-03-05 21:44:39 -0600

    Add files via upload
    NEW CRUD OPERATIONS

--- frontend/src/components/crudServices.vue
+++ frontend/src/components/crudServices.vue
@@ -8,7 +8,35 @@ export default {
   data() {
     return {
       service: "",
+      services: [],
     };
+  },
+  async created() {
+    await this.fetchServices();
+  },
+  methods: {
+    async fetchServices() {
+      try {
+        const response = await axios.get(`${apiURL}/services`);
+        this.services = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addService() {
+      try {
+        const response = await axios.post(`${apiURL}/services`, {
+          service: this.service,
+        });
+        this.services.push(response.data);
+        this.service = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  validations: {
+    service: { required },
   },
 };
 </script>
@@ -21,11 +49,8 @@ export default {
       Services
     </h1>
     <div class="px-10 py-20">
-      <!-- @submit.prevent stops the submit event from reloading the page-->
-      <form @submit.prevent="registerService">
-        <!-- grid container -->
+      <form @submit.prevent="addService">
         <div class="flex h-screen justify-center">
-          <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Description</span>
@@ -33,12 +58,29 @@ export default {
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="service"
+                v-model.trim="$v.service.$model"
               />
+              <div class="text-red-600">
+                {{ $v.service.$error ? "Service description is required" : "" }}
+              </div>
             </label>
+            <button
+              type="submit"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            >
+              Add Service
+            </button>
           </div>
         </div>
       </form>
+      <div class="mt-10">
+        <h2 class="font-bold text-2xl mb-4">List of Services</h2>
+        <ul class="list-disc pl-5">
+          <li v-for="service in services" :key="service.id">
+            {{ service.service }}
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
