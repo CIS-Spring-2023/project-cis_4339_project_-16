f31731bf1d6c14f813cb035dc8af59737ed72df7
Jeremy <jeremy.l.griffith@gmail.com>
2023-03-08 13:26:08 -0600

    OK with the help of ChatGPT, professors lessons, and research I was able to get the Services page up and running.  I am using local storage for the data in Services for right now until we get a back end.  My styling isnt quite done, so the interface isnt pretty.  I need to get the data from this page to display in the intakeevents vue.  I have soft delete functionality using a deactive radio button

--- frontend/src/components/crudServices.vue
+++ frontend/src/components/crudServices.vue
@@ -1,11 +1,54 @@
-<script>
-import useVuelidate from "@vuelidate/core";
-import { required, email, alpha, numeric } from "@vuelidate/validators";
+<template>
+  <main class="flex flex-col justify-center items-center">
+    <h1 class="font-bold text-4xl text-red-700 tracking-widest mb-10">Services</h1>
+    <div class="flex flex-col items-center mb-10">
+      <form @submit.prevent="addService" class="w-full">
+        <div class="flex items-center">
+          <label class="mr-2">
+            <span class="text-gray-700">Description</span>
+            <span style="color: #ff0000">*</span>
+          </label>
+          <input
+            type="text"
+            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            v-model="service.description"
+          />
+          <div class="text-red-600 ml-2">{{ service.descriptionError }}</div>
+          <button
+            type="submit"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded"
+          >
+            Add Service
+          </button>
+        </div>
+      </form>
+      <div class="mt-10">
+        <h2 class="font-bold text-2xl mb-4">List of Services</h2>
+        <ul class="list-disc pl-5">
+          <li v-for="service in activeServices" :key="service.id">
+            {{ service.description }}
+            <button @click="deleteService(service)">
+              {{ service.isActive ? "Deactivate" : "Activate" }}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>
 
+
+
+<script>
 export default {
   data() {
     return {
-      service: "",
+      service: {
+        id: null,
+        description: "",
+        isActive: true,
+        descriptionError: "",
+      },
       services: [],
     };
   },
@@ -14,76 +57,44 @@ export default {
   },
   methods: {
     async fetchServices() {
-      try {
-        const response = await fetch("/services");
-        this.services = await response.json();
-      } catch (error) {
-        console.error(error);
+      const storedServices = localStorage.getItem("services");
+      if (storedServices) {
+        this.services = JSON.parse(storedServices);
       }
     },
     async addService() {
-      if (!this.$v.service.$invalid) {
-        try {
-          const response = await fetch("/services", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ service: this.service }),
-          });
-          const data = await response.json();
-          this.services.push(data);
-          this.service = "";
-        } catch (error) {
-          console.error(error);
-        }
+      if (!this.service.description) {
+        this.service.descriptionError = "Service description is required";
+        return;
+      }
+      this.service.id = new Date().getTime();
+      this.services.push(this.service);
+      localStorage.setItem("services", JSON.stringify(this.services));
+      this.service = {
+        id: null,
+        description: "",
+        isActive: true,
+        descriptionError: "",
+      };
+    },
+    deleteService(service) {
+      const index = this.services.findIndex((s) => s.id === service.id);
+      if (index !== -1) {
+        const updatedService = { ...service, isActive: !service.isActive };
+        this.services.splice(index, 1, updatedService);
+        localStorage.setItem("services", JSON.stringify(this.services));
       }
     },
   },
-  validations: {
-    service: { required },
+  computed: {
+    activeServices() {
+      return this.services.filter((s) => s.isActive);
+    },
+  },
+  watch: {
+    services() {
+      this.activeServices = this.services.filter((s) => s.isActive);
+    },
   },
 };
 </script>
-
-<template>
-  <main>
-    <h1
-      class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-    >
-      Services
-    </h1>
-    <div class="px-10 py-20">
-      <form @submit.prevent="addService">
-        <div class="flex h-screen justify-center">
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Description</span>
-              <span style="color: #ff0000">*</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="service.$model"
-              />
-              <div class="text-red-600">
-                {{ service.$error ? "Service description is required" : "" }}
-              </div>
-            </label>
-            <button
-              type="submit"
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-            >
-              Add Service
-            </button>
-          </div>
-        </div>
-      </form>
-      <div class="mt-10">
-        <h2 class="font-bold text-2xl mb-4">List of Services</h2>
-        <ul class="list-disc pl-5">
-          <li v-for="service in services" :key="service.id">
-            {{ service.service }}
-          </li>
-        </ul>
-      </div>
-    </div>
-  </main>
-</template>
