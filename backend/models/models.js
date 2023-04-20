9749dd2a167f07c3cfb6748ccb707079a8d6ae5f
Distroyed <91142538+Distroyed@users.noreply.github.com>
2023-04-20 18:51:33 -0500

    add users.js file, also added user model for mongodb

--- backend/models/models.js
+++ backend/models/models.js
@@ -129,6 +129,7 @@ const eventDataSchema = new Schema(
   }
 )
 
+// collection for services
 const serviceDataSchema = new Schema(
     {
         _id: {
@@ -156,11 +157,41 @@ const serviceDataSchema = new Schema(
     }
 )
 
+// collection for users
+const userDataSchema = new Schema(
+    {
+        _id: {
+            type: String,
+            default: uuid.v1},
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            required: true
+        },
+        orgs: {
+            type: [{ type: String, ref: 'org'}],
+            required: true,
+            // validate: [(org) => org.length > 0, 'needs at least one org']
+        }
+    },
+    {
+        collection: 'user'
+    }
+)
+
 // create models from mongoose schemas
 const clients = mongoose.model('client', clientDataSchema)
 const orgs = mongoose.model('org', orgDataSchema)
 const events = mongoose.model('event', eventDataSchema)
 const services = mongoose.model('service', serviceDataSchema)
+const users = mongoose.model('user', userDataSchema)
 
 // package the models in an object to export
-module.exports = { clients, orgs, events, services }
+module.exports = { clients, orgs, events, services, users }
---
+++ backend/routes/users.js
@@ -0,0 +1,129 @@
+const express = require('express')
+const router = express.Router()
+
+const org = process.env.ORG
+
+// importing data model schemas
+const { services } = require('../models/models')
+
+// GET 10 most recent services for org
+router.get('/', (req, res, next) => {
+  services
+    .find({ orgs: org }, (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        return res.json(data)
+      }
+    })
+    .sort({ updatedAt: -1 })
+    .limit(10)
+})
+
+// GET single service by ID
+router.get('/id/:id', (req, res, next) => {
+  // use findOne instead of find to not return array
+  services.findOne({ _id: req.params.id, orgs: org }, (error, data) => {
+    if (error) {
+      return next(error)
+    } else if (!data) {
+      res.status(400).send('Service not found')
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// GET entries based on search query
+// Ex: '...?firstName=Bob&lastName=&searchBy=name'
+router.get('/search', (req, res, next) => {
+  const dbQuery = { orgs: org }
+  switch (req.query.searchBy) {
+    case 'name':
+      dbQuery.servicename = { $regex: `^${req.query.servicename}`, $options: 'i' }
+      break
+    case 'servicestatus':
+      dbQuery.status = { $regex: `^${req.query.status}`, $options: 'i' }
+      break
+    default:
+      return res.status(400).send('invalid searchBy')
+  }
+  services.find(dbQuery, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// POST new service
+router.post('/', (req, res, next) => {
+  const newService = req.body
+  newService.orgs = [org]
+  services.create(newService, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// PUT update service
+router.put('/update/:id', (req, res, next) => {
+  services.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// PUT add existing service to org
+router.put('/register/:id', (req, res, next) => {
+  services.findByIdAndUpdate(
+    req.params.id,
+    { $push: { orgs: org } },
+    (error, data) => {
+      if (error) {
+        console.log(error)
+        return next(error)
+      } else {
+        res.send('Service registered with org')
+      }
+    }
+  )
+})
+
+// PUT remove existing service from org
+router.put('/deregister/:id', (req, res, next) => {
+  services.findByIdAndUpdate(
+    req.params.id,
+    { $pull: { orgs: org } },
+    (error, data) => {
+      if (error) {
+        console.log(error)
+        return next(error)
+      } else {
+        res.send('Service deregistered with org')
+      }
+    }
+  )
+})
+
+// hard DELETE service by ID, as per project specifications
+router.delete('/:id', (req, res, next) => {
+  services.findByIdAndDelete(req.params.id, (error, data) => {
+    if (error) {
+      return next(error)
+    } else if (!data) {
+      res.status(400).send('Service not found')
+    } else {
+      res.send('Service deleted')
+    }
+  })
+})
+
+module.exports = router
