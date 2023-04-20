268016b3282637ce68f2217bc12a5e3ba5e9f080
Distroyed <91142538+Distroyed@users.noreply.github.com>
2023-04-20 18:55:17 -0500

    add users.js file, also added user model for mongodb

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -4,11 +4,11 @@ const router = express.Router()
 const org = process.env.ORG
 
 // importing data model schemas
-const { services } = require('../models/models')
+const { users } = require('../models/models')
 
 // GET 10 most recent services for org
 router.get('/', (req, res, next) => {
-  services
+  users
     .find({ orgs: org }, (error, data) => {
       if (error) {
         return next(error)
@@ -23,11 +23,11 @@ router.get('/', (req, res, next) => {
 // GET single service by ID
 router.get('/id/:id', (req, res, next) => {
   // use findOne instead of find to not return array
-  services.findOne({ _id: req.params.id, orgs: org }, (error, data) => {
+  users.findOne({ _id: req.params.id, orgs: org }, (error, data) => {
     if (error) {
       return next(error)
     } else if (!data) {
-      res.status(400).send('Service not found')
+      res.status(400).send('User not found')
     } else {
       res.json(data)
     }
